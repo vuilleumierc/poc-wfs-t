9ea160d6232fd0272d070bba4354cd1c0e0bc234
Cécile Vuilleumier <cecile.vuilleumier@camptocamp.com>
2023-09-14 14:43:32 +0200

    Add user input color for point symbology

--- server/config/config_geoserver.sh
+++ server/config/config_geoserver.sh
@@ -9,13 +9,29 @@ do
 done
 
 # Add postgis datastore
-until curl -u admin:geoserver -XPOST -T "config/datastore_postgis.xml" -H "Content-type: text/xml" $GEOSERVER_URL"rest/workspaces/geo/datastores" 2> /dev/null
+until curl -u admin:geoserver -XPOST -T "config/datastore/datastore_postgis.xml" -H "Content-type: text/xml" $GEOSERVER_URL"rest/workspaces/geo/datastores" 2> /dev/null
 do
     sleep 2
 done
 
-# Add postgis layer
-until curl -u admin:geoserver -XPOST -T "config/layer_location.xml" -H "Content-type: text/xml" $GEOSERVER_URL"rest/workspaces/geo/featuretypes"
+# Add postgis point layer
+until curl -u admin:geoserver -XPOST -T "config/layers/layer_location.xml" -H "Content-type: text/xml" $GEOSERVER_URL"rest/workspaces/geo/featuretypes"
+do
+    sleep 2
+done
+
+# Add point style
+until curl -v -u admin:geoserver -XPOST -H "Content-type: text/xml" -d "<style><name>color_point</name><filename>color_point.sld</filename></style>" $GEOSERVER_URL"rest/workspaces/geo/styles/"
+do
+    sleep 2
+done
+until curl -v -u admin:geoserver -XPUT -H "Content-type: application/vnd.ogc.sld+xml" -T "config/styles/color_point.sld" $GEOSERVER_URL"rest/workspaces/geo/styles/color_point"
+do
+    sleep 2
+done
+
+# Link style to layer
+until curl -v -u admin:geoserver -XPUT -H "Content-type: text/xml" -d "<layer><defaultStyle><name>color_point</name></defaultStyle></layer>" $GEOSERVER_URL"rest/workspaces/geo/layers/location"
 do
     sleep 2
 done
---
+++ server/config/styles/color_point.sld
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<StyledLayerDescriptor version="1.0.0"
+  xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.0.0/StyledLayerDescriptor.xsd"
+  xmlns="http://www.opengis.net/sld" xmlns:ogc="http://www.opengis.net/ogc"
+  xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+
+  <NamedLayer>
+    <Name>color_point</Name>
+    <UserStyle>
+      <Title>Points with color</Title>
+      <FeatureTypeStyle>
+        <Rule>
+          <Title>points</Title>
+          <PointSymbolizer>
+            <Graphic>
+              <Mark>
+                <WellKnownName>circle</WellKnownName>
+                <Fill>
+                  <CssParameter name="fill">
+                    <ogc:PropertyName>color</ogc:PropertyName>
+                  </CssParameter>
+                </Fill>
+              </Mark>
+              <Size>10</Size>
+            </Graphic>
+          </PointSymbolizer>
+        </Rule>
+
+      </FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor>

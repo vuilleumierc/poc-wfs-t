fbc239542fedfef3150f443ad4d84251a7f2ebb5
Cécile Vuilleumier <cecile.vuilleumier@camptocamp.com>
2023-09-25 15:20:13 +0200

    Add point symbology which uses SVG icons (remote and local)

--- server/config/config_geoserver.sh
+++ server/config/config_geoserver.sh
@@ -3,27 +3,27 @@
 # Script to initialize geoserver config on composition start-up using the REST API
 
 # Create a workspace
-until curl -u admin:geoserver -POST -H "Content-type: text/xml"  -d "<workspace><name>geo</name></workspace>"  $GEOSERVER_URL"rest/workspaces" 2> /dev/null
+until curl -v -u admin:geoserver -POST -H "Content-type: text/xml"  -d "<workspace><name>geo</name></workspace>"  $GEOSERVER_URL"rest/workspaces"
 do
     sleep 2
 done
 
 # Add postgis datastore
-until curl -u admin:geoserver -XPOST -T "config/datastore/datastore_postgis.xml" -H "Content-type: text/xml" $GEOSERVER_URL"rest/workspaces/geo/datastores" 2> /dev/null
+until curl -v -u admin:geoserver -XPOST -T "config/datastore/datastore_postgis.xml" -H "Content-type: text/xml" $GEOSERVER_URL"rest/workspaces/geo/datastores"
 do
     sleep 2
 done
 
 # Add postgis layers
-until curl -u admin:geoserver -XPOST -T "config/layers/layer_location.xml" -H "Content-type: text/xml" $GEOSERVER_URL"rest/workspaces/geo/featuretypes"
+until curl -v -u admin:geoserver -XPOST -T "config/layers/layer_location.xml" -H "Content-type: text/xml" $GEOSERVER_URL"rest/workspaces/geo/featuretypes"
 do
     sleep 2
 done
-until curl -u admin:geoserver -XPOST -T "config/layers/layer_line.xml" -H "Content-type: text/xml" $GEOSERVER_URL"rest/workspaces/geo/featuretypes"
+until curl -v -u admin:geoserver -XPOST -T "config/layers/layer_line.xml" -H "Content-type: text/xml" $GEOSERVER_URL"rest/workspaces/geo/featuretypes"
 do
     sleep 2
 done
-until curl -u admin:geoserver -XPOST -T "config/layers/layer_area.xml" -H "Content-type: text/xml" $GEOSERVER_URL"rest/workspaces/geo/featuretypes"
+until curl -v -u admin:geoserver -XPOST -T "config/layers/layer_area.xml" -H "Content-type: text/xml" $GEOSERVER_URL"rest/workspaces/geo/featuretypes"
 do
     sleep 2
 done
@@ -58,7 +58,7 @@ do
     sleep 2
 done
 
-# Link style to layer
+# Link style to layer (default)
 until curl -v -u admin:geoserver -XPUT -H "Content-type: text/xml" -d "<layer><defaultStyle><name>color_point</name></defaultStyle></layer>" $GEOSERVER_URL"rest/workspaces/geo/layers/location"
 do
     sleep 2
@@ -68,6 +68,29 @@ do
     sleep 2
 done
 until curl -v -u admin:geoserver -XPUT -H "Content-type: text/xml" -d "<layer><defaultStyle><name>color_polygon</name></defaultStyle></layer>" $GEOSERVER_URL"rest/workspaces/geo/layers/area"
+do
+    sleep 2
+done
+
+# Add svg styles
+until curl -v -u admin:geoserver -XPOST -H "Content-type: text/xml" -d "<style><name>svg_local_point</name><filename>svg_local_point.sld</filename></style>" $GEOSERVER_URL"rest/workspaces/geo/styles/"
+do
+    sleep 2
+done
+until curl -v -u admin:geoserver -XPUT -H "Content-type: application/vnd.ogc.sld+xml" -T "config/styles/svg_local_point.sld" $GEOSERVER_URL"rest/workspaces/geo/styles/svg_local_point"
+do
+    sleep 2
+done
+until curl -v -u admin:geoserver -XPOST -H "Content-type: text/xml" -d "<style><name>svg_online_point</name><filename>svg_online_point.sld</filename></style>" $GEOSERVER_URL"rest/workspaces/geo/styles/"
+do
+    sleep 2
+done
+until curl -v -u admin:geoserver -XPUT -H "Content-type: application/vnd.ogc.sld+xml" -T "config/styles/svg_online_point.sld" $GEOSERVER_URL"rest/workspaces/geo/styles/svg_online_point"
+do
+    sleep 2
+done
+
+until curl -v -u admin:geoserver -XPUT -H "Content-type: text/xml" -d "<layer><styles><style><name>svg_local_point</name></style><style><name>svg_online_point</name></style></styles></layer>" $GEOSERVER_URL"rest/workspaces/geo/layers/location"
 do
     sleep 2
 done
